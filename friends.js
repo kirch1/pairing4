e92ccdaee3cfa7bc73403ffc8d496af7258b8842
Jason Kirchman <kirchman.jason@icloud.com>
2022-10-25 21:47:20 -0500

    Add second for statement

--- friends.js
+++ friends.js
@@ -4,5 +4,10 @@ for (var i = 0; i < numbers.length; i++) {
     if (numbers[i] > 30) {
         console.log(numbers[i]);
     }
-};
+}
 
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 60) {
+        console.log(numbers[i]);
+    }
+}
